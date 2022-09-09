e3fdd86b33fdf715dd8179086879cee7ab5ce0d2
unknown <no.8602775059@gmail.com>
2022-09-09 15:13:09 +0530

    Mysql to Postgresql

--- myproject/settings.py
+++ myproject/settings.py
@@ -77,22 +77,22 @@ WSGI_APPLICATION = 'myproject.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-#DATABASES = {
-#    'default': {
-#        'ENGINE': 'django.db.backends.postgresql',
-#        'NAME': 'myproject',
-#        'USER': 'postgres',
-#        'PASSWORD': 'Abhishek',
-#        'HOST': 'localhost'
-#    }
-#}
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'myproject',
+        'USER': 'postgres',
+        'PASSWORD': 'Abhishek',
+        'HOST': 'localhost'
     }
 }
+
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': BASE_DIR / 'db.sqlite3',
+#    }
+#}
 
 
 # Password validation
