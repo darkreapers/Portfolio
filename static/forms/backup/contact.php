216777a214d0b5853d66d929d08cc224ee791c17
unknown <no.8602775059@gmail.com>
2022-09-11 20:33:09 +0530

    Sections removed

---
+++ static/forms/backup/contact.php
@@ -0,0 +1,41 @@
+{% load static %}
+
+<?php
+  /**
+  * Requires the "PHP Email Form" library
+  * The "PHP Email Form" library is available only in the pro version of the template
+  * The library should be uploaded to: vendor/php-email-form/php-email-form.php
+  * For more info and help: https://bootstrapmade.com/php-email-form/
+  */
+
+  // Replace contact@example.com with your real receiving email address
+  $receiving_email_address = 'no.8602775059@gmail.com';
+
+  if( file_exists($php_email_form = '..static/assets/vendor/php-email-form/php-email-form.php' )) {
+    include( $php_email_form );
+  } else {
+    die( 'Unable to load the "PHP Email Form" Library!');
+  }
+
+  $contact = new PHP_Email_Form;
+  $contact->ajax = true;
+  
+  $contact->to = $receiving_email_address;
+  $contact->from_name = $_POST['name'];
+  $contact->from_email = $_POST['email'];
+  $contact->subject = $_POST['subject'];
+
+  // Uncomment below code if you want to use SMTP to send emails. You need to enter your correct SMTP credentials
+  
+  $contact->smtp = array(
+    'host' => 'http://sandboxaf2a025b54044805be532f43d383a4a6.mailgun.org/',
+    'user' => '2ede45bf19f1a700da22cc4fbd1ca02d-07a637b8-37973753',
+  );
+  
+
+  $contact->add_message( $_POST['name'], 'From');
+  $contact->add_message( $_POST['email'], 'Email');
+  $contact->add_message( $_POST['message'], 'Message', 10);
+
+  echo $contact->send();
+?>
--- static/forms/contact.php
+++ static/forms/contact.php
@@ -9,35 +9,20 @@
   */
 
   // Replace contact@example.com with your real receiving email address
-  $receiving_email_address = 'no.8602775059@gmail.com';
+  $receiving_email_address = 'no.8602775059@gmail.com'; echo 1
 
   if( file_exists($php_email_form = '..static/assets/vendor/php-email-form/php-email-form.php' )) {
-    include( $php_email_form );
+    include( $php_email_form ); echo 2
   } else {
-    die( 'Unable to load the "PHP Email Form" Library!');
+    die( 'Unable to load the "PHP Email Form" Library!'); echo 3
   }
 
-  $contact = new PHP_Email_Form;
-  $contact->ajax = true;
-  
-  $contact->to = $receiving_email_address;
-  $contact->from_name = $_POST['name'];
-  $contact->from_email = $_POST['email'];
-  $contact->subject = $_POST['subject'];
 
-  // Uncomment below code if you want to use SMTP to send emails. You need to enter your correct SMTP credentials
-  /*
-  $contact->smtp = array(
-    'host' => 'example.com',
-    'username' => 'example',
-    'password' => 'pass',
-    'port' => '587'
-  );
-  */
+$mg = Mailgun::create('2ede45bf19f1a700da22cc4fbd1ca02d-07a637b8-37973753'); echo 4 // For US servers 
 
-  $contact->add_message( $_POST['name'], 'From');
-  $contact->add_message( $_POST['email'], 'Email');
-  $contact->add_message( $_POST['message'], 'Message', 10);
-
-  echo $contact->send();
-?>
+$mg->messages()->send('http://sandboxaf2a025b54044805be532f43d383a4a6.mailgun.org/', [
+  'from'    => $_POST['email'],
+  'to'      => 'no.8602775059@gmail.com',
+  'subject' => $_POST['subject'],
+  'text'    => $_POST['message']
+]) echo 5
